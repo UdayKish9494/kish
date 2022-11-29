0d0ca28e8142aa76a16ffb9cff2eefa63f22a9da
Acer <Acer@DESKTOP-GFJL6LQ>
2022-11-29 13:20:30 +0530

    My commit of project

---
+++ RestAssuredDemo/src/test/java/demo/Demo1_get.java
@@ -0,0 +1,33 @@
+package demo;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class Demo1_get {
+	
+	@Test
+	public void getweather() {
+		
+		
+		
+		 Response response = get("https://reqres.in/api/users?page=2");
+		 
+		 System.out.println(response.asString());
+		 System.out.println(response.getStatusCode());
+		 System.out.println(response.getStatusLine());
+		 System.out.println(response.getBody().asString());
+		 System.out.println(response.getTime());
+		 
+		 
+		 
+	}
+	}
+
+	
+
+
