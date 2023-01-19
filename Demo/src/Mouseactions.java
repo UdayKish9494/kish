c755b19c0abe6a954d5c43c98de483c1ade9235c
Acer <Acer@DESKTOP-GFJL6LQ>
2023-01-19 12:40:19 +0530

    commit

--- Demo/src/Mouseactions.java
+++ Demo/src/Mouseactions.java
@@ -1,4 +1,10 @@
+import java.io.File;
+import java.io.IOException;
+
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -6,9 +12,11 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
+import com.google.common.io.Files;
+
 public class Mouseactions {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
         String baseUrl = "http://demo.guru99.com/test/newtours/";
         System.setProperty("webdriver.chrome.driver","C://Users//Acer//Downloads//chromedriver_win32 (1)//chromedriver.exe");
                 WebDriver driver = new ChromeDriver();
@@ -34,6 +42,17 @@ public class Mouseactions {
                 bgColor = td_Home.getCssValue("background-color");
                 System.out.println("After hover: " + bgColor);
                 driver.close();
+                
+                TakesScreenshot ts = (TakesScreenshot)driver; 
+        		File scrfile = ts.getScreenshotAs(OutputType.FILE);
+        		File destfile = newFile("C:\\Users\\Acer\\Desktop\\screenshot\\newpic.png");
+        		Files.copy(scrfile, destfile);
+        		
         }
+
+	private static File newFile(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
